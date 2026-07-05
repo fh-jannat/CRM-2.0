28687cbd5624b888be2943f79567dc5ad0456fb9
fh-jannat <fh.jannat07@gmail.com>
2026-07-05 15:02:18 +0600

    Initial Commit

--- backend/src/modules/shared/database/typeorm1/typeorm.module.ts
+++ backend/src/modules/shared/database/typeorm1/typeorm.module.ts
@@ -8,16 +8,27 @@ import { join } from 'path';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('PG_HOST'),
-        port: configService.get<number>('PG_PORT'),
-        username: configService.get<string>('PG_USER'),
-        password: configService.get<string>('PG_PASSWORD'),
-        database: configService.get<string>('PG_DB'),
-        entities: [join(__dirname, '**', '*.entity.{ts,js}')],
-        synchronize: true,
-        logging: true,
-      }),
+  type: 'postgres',
+  host: configService.get<string>('PG_HOST'),
+  port: configService.get<number>('PG_PORT'),
+  username: configService.get<string>('PG_USER'),
+  password: configService.get<string>('PG_PASSWORD'),
+  database: configService.get<string>('PG_DB'),
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
+
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+
+  entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+  synchronize: true,
+  logging: true,
+}),
       inject: [ConfigService],
     }),
   ],
